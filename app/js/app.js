11cb8c8d8b58afc4282994a55b56b5b36d44cd69
Unbyw <contact.florian.ortiz@gmail.com>
2014-08-15 12:16:26 +0200

    controllers struct

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,5 @@
 /**
  * Created by FORTIZ on 8/1/14.
  */
-var avy = angular.module('avy'['xx'
+var avy = angular.module('avy',[avyControllers
     ]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,3 +1,28 @@
 /**
  * Created by FORTIZ on 8/1/14.
  */
+var avyControllers = angular.modules();
+
+avyControllers.controller('taskListCtrl', '$scope', '$http', function($scope, $http){
+
+});
+
+avyControllers.controller('newTaskCtrl', '$scope', '$http', function($scope, $http){
+
+});
+
+avyControllers.controller('taskDtlCtrl', '$scope', '$http', function($scope, $http){
+
+});
+
+avyControllers.controller('kpiListCtrl', '$scope', '$http', function($scope, $http){
+
+});
+
+avyControllers.controller('kpiDtlCtrl', '$scope', '$http', function($scope, $http){
+
+});
+
+avyControllers.controller('newKpiCtrl', '$scope', '$http', function($scope, $http){
+
+});
